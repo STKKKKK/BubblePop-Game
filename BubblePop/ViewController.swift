57d92ed07c676b95faa71885528ea1a65e1e0070
skk <skk@192.168.0.112>
2021-01-16 17:08:54 +0800

    Initial Commit

---
+++ BubblePop/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BubblePop
+//
+//  Created by skk on 2021/1/16.
+//  Copyright © 2021 Hong Kung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hello git")
+    }
+
+
+}
+
