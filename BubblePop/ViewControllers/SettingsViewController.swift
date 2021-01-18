34a602fec130b0579c8e7a41a40910e4019e602b
skk <skk@192.168.0.112>
2021-01-18 22:40:39 +0800

    new

---
+++ BubblePop/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BubblePop
+//
+//  Created by skk on 2021/1/18.
+//  Copyright © 2021 Hong Kung. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("settings page")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
