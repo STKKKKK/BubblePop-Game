a57a16c3f7683e7fce5b32b0e71c080276fe1d26
skk <skk@192.168.0.112>
2021-01-20 15:07:42 +0800

    new

---
+++ BubblePop/ViewControllers/ScoreboardViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BubblePop
+//
+//  Created by skk on 2021/1/20.
+//  Copyright © 2021 Hong Kung. All rights reserved.
+//
+
+import UIKit
+
+class ScoreboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
