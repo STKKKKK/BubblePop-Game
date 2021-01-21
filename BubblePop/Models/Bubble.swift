ab514de6b1494e7b9de5111167e397dc34ef8256
skk <skk@192.168.0.112>
2021-01-22 00:24:15 +0800

    new

--- BubblePop/Models/Bubble.swift
+++ BubblePop/Models/Bubble.swift
@@ -11,37 +11,70 @@ import UIKit
 
 class Bubble: UIButton {
     
-    let size = 80
+    let size = 50
+    var score = 0
     
-    init(x: Int, y: Int, color: UIColor) {
+    init(x: Int, y: Int) {
         super.init(frame: CGRect(x: x, y: y, width: size, height: size))
-        self.backgroundColor = color
+        self.layer.cornerRadius = 0.5 * self.bounds.size.width
+        
+        switch Int.random(in: 1...100) {
+        case 1...40:
+            self.backgroundColor = .red
+            score = 1
+        case 41...70:
+            self.backgroundColor = .systemPink
+            score = 2
+        case 71...85:
+            self.backgroundColor = .green
+            score = 5
+        case 86...95:
+            self.backgroundColor = .blue
+            score = 8
+        default:
+            self.backgroundColor = .black
+            score = 10
+        }
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func appear() {
-        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
-            self.alpha = 0.8
-        })
+    override func addTarget(self, action: Selector, for controlEvents: UIControl.Event) {
+        <#code#>
     }
     
-    func disappear() {
-        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
-            self.alpha = 0
-        }) { (_) in
-            self.removeFromSuperview()
+//    func gainScore() -> Int {
+//        return score
+//    }
+    
+    @IBAction func bubblePressed(_ sender: UIButton) {
+        sender.removeFromSuperview()
+        // update the player's score
         }
     }
     
-    func pop() {
-        UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut, animations: {
-            self.transform = CGAffineTransform(scaleX: 0.05, y: 0.05)
-        }) { (_) in
-            self.removeFromSuperview()
-        }
-    }
+//    func appear() {
+//        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
+//            self.alpha = 0.8
+//        })
+//    }
+//
+//    func disappear() {
+//        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
+//            self.alpha = 0
+//        }) { (_) in
+//            self.removeFromSuperview()
+//        }
+//    }
+//
+//    func pop() {
+//        UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut, animations: {
+//            self.transform = CGAffineTransform(scaleX: 0.05, y: 0.05)
+//        }) { (_) in
+//            self.removeFromSuperview()
+//        }
+//    }
     
 }
--- BubblePop/ViewControllers/GameViewController.swift
+++ BubblePop/ViewControllers/GameViewController.swift
@@ -13,52 +13,132 @@ class GameViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var highScoreLabel: UILabel!
-    @IBOutlet weak var BubblesView: UIView!
+    @IBOutlet weak var bubblesView: UIView!
+    
     
     var player: String?
-    var timer: Timer?
+    var timer = Timer()
     var timeLeft = 0
     var score = 0
     
-
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//        let defaults = UserDefaults.standard
+//        player = defaults.string(forKey: "newPlayer")
+//        print("GamePlayViewController:", player!)
+//
+//        let highScore = defaults.integer(forKey: "highScore")
+//        highScoreLabel.text = String(highScore)
+//
+//        let settings = Settings()
+//        timeLeft = settings.gameTime
+//        timeLabel.text = String(timeLeft)
+//        scoreLabel.text = String(score)
+//    }
+//
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(true)
+//
+//        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+//            self.updateTimeLabel()
+//            self.updateBubbles()
+//        })
+//        timer?.fire()
+//    }
+//
+//    func updateTimeLabel() {
+//        if timeLeft > 0 {
+//            timeLeft -= 1
+//            timeLabel.text = String(timeLeft)
+//        } else {
+//            timer?.invalidate()
+//            self.performSegue(withIdentifier: "scoreboardSegue", sender: nil)
+//        }
+//    }
+//
+//    func updateBubbles() {
+//
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         let defaults = UserDefaults.standard
         player = defaults.string(forKey: "newPlayer")
         print("GamePlayViewController:", player!)
-        
+
         let highScore = defaults.integer(forKey: "highScore")
         highScoreLabel.text = String(highScore)
-        
+
         let settings = Settings()
         timeLeft = settings.gameTime
         timeLabel.text = String(timeLeft)
         scoreLabel.text = String(score)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(true)
         
-        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
-            self.updateTimeLabel()
-            self.updateBubbles()
-        })
-        timer?.fire()
+        let a = Int(bubblesView.frame.minX)
+        let b = Int(bubblesView.frame.maxX)
+        let c = Int(bubblesView.frame.minY)
+        let d = Int(bubblesView.frame.maxY)
+        print(a, b, c, d)
+
+        //timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(counter), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {
+            timer in
+            self.counter()
+            //self.printing()
+            self.generateBubble()
+            //self.removeBubble()
+        }
+        
     }
     
-    func updateTimeLabel() {
-        if timeLeft > 0 {
-            timeLeft -= 1
-            timeLabel.text = String(timeLeft)
-        } else {
-            timer?.invalidate()
-            self.performSegue(withIdentifier: "scoreboardSegue", sender: nil)
+    func counter() {
+        timeLeft -= 1
+        timeLabel.text = String(timeLeft)
+        
+        if timeLeft == 0 {
+            timer.invalidate()
+            print("Game is over!")
+            
+//            let vc = storyboard?.instantiateViewController(identifier: "GreenViewController") as! GreenViewController
+//            present(vc, animated: true, completion: nil)
         }
     }
     
-    func updateBubbles() {
+    @objc func printing() {
+        print("The timer is counting down!")
+    }
+    
+    @objc func generateBubble() {
+        // create bubble class
+        let bubble = UIButton()
+        
+        let a = Int(bubblesView.frame.minX)
+        
+//        let xAxis = Int.random(in: 20...200)
+//        let yAxis = Int.random(in: 20...200)
+        let xAxis = Int.random(in: 20...200)
+        let yAxis = Int.random(in: 20...200)
+
+        bubble.frame = CGRect(x: xAxis, y: yAxis, width: 50, height: 50)
+        bubble.layer.cornerRadius = 0.5 * bubble.bounds.size.width
+        bubble.backgroundColor = .red
+        bubble.addTarget(self, action: #selector(bubblePressed), for: .touchUpInside)
+        
+        self.bubblesView.addSubview(bubble)
         
     }
+    
+    @IBAction func bubblePressed(_ sender: UIButton) {
+        sender.removeFromSuperview()
+        // update the player's score
+        print("This bubble is pressed")
+    }
+    
+//    @IBAction func goToHighScore(_ sender: UIButton) {
+//        let vc = storyboard?.instantiateViewController(identifier: "GreenViewController") as! GreenViewController
+//        present(vc, animated: true, completion: nil)
+//    }
     
 }
