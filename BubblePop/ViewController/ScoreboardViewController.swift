1244e281eb48a748fd5def80e22d7999cbcd8f47
skk <skk@192.168.0.112>
2021-01-25 14:44:17 +0800

    new

--- BubblePop/ViewController/ScoreboardViewController.swift
+++ BubblePop/ViewController/ScoreboardViewController.swift
@@ -10,10 +10,31 @@ import UIKit
 
 class ScoreboardViewController: UIViewController {
 
+    @IBOutlet weak var scoreTabelView: UITableView!
+    
+    let recordStorage = RecordStorage()
+    var records: [Record] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        records = bubbleSortRecords(recordStorage.getDecodeRecords())
+    }
+}
+
+
+extension ScoreboardViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return records.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "scoreCell", for: indexPath)
 
+        let index = indexPath.row
+        let record = records[index]
+
+        cell.textLabel?.text = record.player
+        cell.detailTextLabel?.text = String(record.score)
+        return cell
+    }
 }
